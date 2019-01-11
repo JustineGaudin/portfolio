e8bf709c0974668eab3dfd8931dca2b52fb7f74c
Justine Gaudin <37076186+JustineGaudin@users.noreply.github.com>
2019-01-11 17:09:40 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,3 +5,22 @@ let balanceTeam = ["Project:", "Interflora", "Wethere", "Spotify", "Uny"];
 for (let name of balanceTeam) {
   console.log(name);
 }
+
+let button = document.querySelector("button");
+
+let nbLike = 0;
+
+button.addEventListener("click", () => {
+  button.style.animation = "blink 0.5s linear 2 alternate";
+
+  nbLike++;
+  if (nbLike > 1) {
+    button.innerHTML = nbLike + " likes";
+  } else {
+    button.innerHTML = nbLike + " like";
+  }
+});
+
+button.addEventListener("animationend", () => {
+  button.style.animation = "none";
+});
