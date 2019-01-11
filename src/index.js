65862fe1b6eb104f9ac7fc79c6207f4187a08e75
Justine Gaudin <37076186+JustineGaudin@users.noreply.github.com>
2019-01-11 14:55:22 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Im in the index.js");
+let anArrayOfAnything = ["Justine", "Gaudin", "UX/UI Designer"];
+console.log(anArrayOfAnything);
+
+let balanceTeam = ["Project:", "Interflora", "Wethere", "Spotify", "Uny"];
+for (let name of balanceTeam) {
+  console.log(name);
+}
